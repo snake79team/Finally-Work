2312b76d8ac235d57addfe0c6a7b66c79c7e0aab
Vladislav <overnight79@mail.ru>
2024-04-28 21:22:38 +0300

    Дописал программу

--- Program.cs
+++ Program.cs
@@ -4,3 +4,35 @@ class Program
 {
     static string[] FilterStrings(string[] arr)
     {
+                int count = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] newArr = new string[count];
+        count = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3)
+            {
+                newArr[count] = arr[i];
+                count++;
+            }
+        }
+
+        return newArr;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Введите строки через пробел:");
+        string[] input = Console.ReadLine().Split(' ');
+
+        string[] filteredArr = FilterStrings(input);
+
+        Console.WriteLine(string.Join(", ", filteredArr));
+    }
+}
